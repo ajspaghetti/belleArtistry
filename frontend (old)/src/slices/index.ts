9c673138e09175ea59a2cd1d7669f29c56aa1e9e
Jandro <71239294+coolcoolcoolyo@users.noreply.github.com>
2024-03-01 15:55:26 -0500

    deleted old frontend folder

--- frontend (old)/src/slices/index.ts
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from '@reduxjs/toolkit';
-import counterReducer from './counterSlice';
-
-const rootReducer = combineReducers({
-  counter: counterReducer,
-  // TODO: add other reducers
-});
-
-export default rootReducer;
